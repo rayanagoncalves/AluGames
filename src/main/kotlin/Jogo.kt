3ac5ffe3f35ff303254c10fc47dbd989a4a99491
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-08-28 16:10:59 -0300

    Desserializando dados do JSON

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,4 +1,7 @@
-class Jogo(val titulo: String, val capa: String) { // construtor
+import com.google.gson.annotations.SerializedName
+
+class Jogo(@SerializedName("title") val titulo: String,
+           @SerializedName("thumb") val capa: String) { // construtor
     val descricao = ""
 
     override fun toString(): String {
