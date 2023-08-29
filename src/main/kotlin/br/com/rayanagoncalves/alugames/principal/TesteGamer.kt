2f3fae240b2c24b088efe0fdba1ff092699ac950
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-08-29 14:59:54 -0300

    Criando classe Gamer, trabalhando com diferentes construtores e com scope functions

---
+++ src/main/kotlin/br/com/rayanagoncalves/alugames/principal/TesteGamer.kt
@@ -0,0 +1,19 @@
+package br.com.rayanagoncalves.alugames.principal
+
+import br.com.rayanagoncalves.alugames.modelo.Gamer
+
+fun main() {
+    val gamer1 = Gamer("Rayana", "rayana@email.com")
+    println(gamer1)
+
+    val gamer2 = Gamer("Ana", "ana@email.com", "12/12/1998", "ana12")
+    println(gamer2)
+
+    gamer1.let { // scope function
+        it.dataNascimento = "12/05/1999"
+        it.usuario = "rayanacunha"
+        it.idInterno = "rayanacunha123"
+    }
+
+    println(gamer1)
+}
