237e22d5b20d11da269e236b0d04456751ee6004
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-08-29 16:23:12 -0300

    Implementando busca de jogos com repetição

--- src/main/kotlin/br/com/rayanagoncalves/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/rayanagoncalves/alugames/principal/Main.kt
@@ -6,44 +6,47 @@ import java.util.*
 
 fun main() {
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = leitura.nextLine()
+    do {
+        println("Digite um código de jogo para buscar:")
+        val busca = leitura.nextLine()
 
-    val buscaApi = ConsumoApi()
+        val buscaApi = ConsumoApi()
 
-    val response = buscaApi.buscaJogo(busca)
+        val response = buscaApi.buscaJogo(busca)
 
-    var meuJogo: Jogo? = null
+        var meuJogo: Jogo? = null
 
-    val resultado = runCatching {
-        val json = response.body()
+        val resultado = runCatching {
+            val json = response.body()
 
-        val gson = Gson()
-        val informacaoJogo = gson.fromJson(json, InfoJogo::class.java)
-        meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb)
-    }
-
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id.")
-    }
-
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? S/N")
-        val opcao = leitura.nextLine()
-
-        if(opcao.equals("s", true)) {
-            println("Insira a descrição personalizada para o jogo:")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        } else {
-            meuJogo?.descricao = meuJogo?.titulo
+            val gson = Gson()
+            val informacaoJogo = gson.fromJson(json, InfoJogo::class.java)
+            meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb)
         }
 
-        println(meuJogo)
-    }
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id.")
+        }
 
-    resultado.onSuccess {
-        println("Busca finalizada com sucesso.")
-    }
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada? S/N")
+            val opcao = leitura.nextLine()
 
+            if(opcao.equals("s", true)) {
+                println("Insira a descrição personalizada para o jogo:")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            println(meuJogo)
+        }
+
+        println("Deseja buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+
+    } while (resposta.equals("S", true))
+
+    println("Busca finalizada com sucesso.")
 }
