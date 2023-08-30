303c12ac0b52b2e229fc2a0d570ef4fb95d88dbb
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-08-30 16:54:58 -0300

    Tornando a classe Plano selada (sealed class)

--- src/main/kotlin/br/com/rayanagoncalves/alugames/modelo/Plano.kt
+++ src/main/kotlin/br/com/rayanagoncalves/alugames/modelo/Plano.kt
@@ -1,8 +1,6 @@
 package br.com.rayanagoncalves.alugames.modelo
 
-open class Plano( // a palavra reservada open é usada para indicar que a classe está aberta para ser herdada
-    val tipo: String
-) {
+sealed class Plano(val tipo: String) { // sealed class segue o mesmo conceito da classe abstrata, ou seja, nao pode ser instanciada, mas a diferença é que ela só pode ser herdada por classes que estejam no mesmo pacote
 
     open fun obterValor(aluguel: Aluguel): Double {
         return aluguel.jogo.preco * aluguel.periodo.emDias
